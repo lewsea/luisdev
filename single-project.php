756cfc041aa498a9581d8d4903e5c1ecf6fe4b82
lewsea <luis.gudmalin@gmail.com>
2021-04-13 23:10:39 +0800

    update single project pagination

--- single-project.php
+++ single-project.php
@@ -9,60 +9,88 @@
 
 get_header();
 ?>
-
 	<main id="primary" class="site-main">
 		<div class="wrapper single-project-wrapper">
 			<div class="container">
-			<?php
-				while ( have_posts() ) :
-					the_post();
+                <?php
+                    while ( have_posts() ) :
+                        the_post();
 
-					get_template_part( 'template-parts/content', get_post_type() );
+                        get_template_part( 'template-parts/content', get_post_type() );
 
-					// the_post_navigation(
-					// 	array(
-					// 		'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'base-underscore' ) . '</span> <span class="nav-title">%title</span>',
-					// 		'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'base-underscore' ) . '</span> <span class="nav-title">%title</span>',
-					// 	)
-					// );
+                    endwhile; // End of the loop.
+                ?>
 
-				endwhile; // End of the loop.
-			?>
+                <div class="single-pagination">
+                    <?php 
+                        if ( !empty($prev = get_previous_post()) ) {
+                            $prev_title = $prev->post_title;
+                            $prev_img = get_the_post_thumbnail_url($prev->ID);
+                    ?>
+                    <div class="prev">
+                        <p>Previous</p>
+                        <a href="<?php echo esc_url( get_permalink( $prev->ID ) ); ?>">
+                            <div class="prev-link">
+                                <i class="fa fa-long-arrow-alt-left"></i>
+                                <h4><?php echo $prev_title; ?></h4>
+                            </div>
+                            
+                            <img src="<?php echo $prev_img; ?>" alt="<?php echo $prev_title; ?>">
+                        </a>
+                    </div>
+                    <?php }?>
 
-            <div class="single-pagination">
-                <?php previous_post_link( $format = '<div class="prev-link"> <i class="fa fa-long-arrow-alt-left"></i> %link </div>' ) ?>
-                <?php next_post_link($format = '<div class="next-link"> %link <i class="fa fa-long-arrow-alt-right"></i> </div>'); ?>
-            </div>
+                    <?php 
+                        if ( !empty($next = get_next_post()) ) {
+                            $next_title = $next->post_title;
+                            $next_img = get_the_post_thumbnail_url($next->ID);
+                        ?>
+                        <div class="next">
+                            <p>Next</p>
+                            <a href="<?php echo esc_url( get_permalink( $next->ID ) ); ?>">
+                                <div class="next-link">
+                                    <h4><?php echo $next_title; ?></h4>
+                                    <i class="fa fa-long-arrow-alt-right"></i>
+                                </div>
+                                <img src="<?php echo $next_img; ?>" alt="<?php echo $next_title; ?>">
+                            </a>
+                        </div>
+                    <?php }; ?>
+                </div>
 
-			<?php
-			$tags = wp_get_post_tags($post->ID);
-			if ($tags) {
-			echo '<h2 class="related-title">More of my Projects</h2>';
-			$first_tag = $tags[0]->term_id;
-			$args=array(
-			'tag__in' => array($first_tag),
-			'post__not_in' => array($post->ID),
-			'posts_per_page'=>3,
-			'caller_get_posts'=>1
-			);
-			$my_query = new WP_Query($args);
-			if( $my_query->have_posts() ) {
-			while ($my_query->have_posts()) : $my_query->the_post(); ?>
+                <div class="container-title">
+                    <h3 class="title">More of my Project</h3>
+                </div>
 
-				<div class="related-blog-block">
-					<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
-						<div class="blog-inner">
-							<h3><?php the_title(); ?></h3>
-							<p><?php echo wp_trim_words(get_the_excerpt(), 20); ?></p>
-						</div>
-					</a>
-				</div>
-			<?php
-			endwhile;
-			}
-			wp_reset_query();
-			}
-			?>
+                <div class="project-container">
+                    <?php 
+                        $args = array (
+                        'post_type' => 'project',
+                        'posts_per_page' => 3,
+                        'orderby' => 'rand',
+                        );
+                        $blogposts = new WP_Query($args);
+                        while ($blogposts->have_posts()) {
+                        $blogposts->the_post();
+                    ?>
+                        <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+                            <div class="entry-content" style="border: none; padding: 0; margin: 0">
+                                <div class="project-content">
+                                <p class="theme-site" ><?php echo the_field('what_theme') ?></p>
+                                <div class="theme-title">
+                                    <a href="<?php the_permalink()?>"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></a>
+                                    <p><?php echo the_field('version') ?></p>
+                                </div> 
+                                <p class="theme-desc" ><?php echo wp_trim_words(get_the_excerpt(), 25); ?></p>
+                                <div class="theme-tags">
+                                    <?php echo the_tags(); ?>
+                                </div>
+                                <a href="<?php the_permalink()?>" class="view-proj" >View Project</a>
+                                </div>	
+                            </div>
+                        </article>
+                    <?php } wp_reset_query(); ?>
+                </div>
 			</div>
 		</div>
 	</main>
