3856ede3d603f14c4081a732003bf407afb30cb1
lewsea <luis.gudmalin@gmail.com>
2021-04-19 21:59:10 +0800

    fix single content-project

--- template-parts/content-project.php
+++ template-parts/content-project.php
@@ -21,33 +21,54 @@
 		<a href="<?php echo the_field('demo_link') ?>" class="demo-link" ><i class="fa fa-eye"></i> Demo</a>
 	</div>
 	<div class="entry-content">
-		<h4 class="entry-desc" >Description</h4>
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'luisdev' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-        ?>
-        <h4 class="entry-desc" >Process</h4>
-        <p><?php echo get_the_excerpt() ?></p>
-        <h4 class="entry-desc" >Tools Used</h4>
-        <div class="tools-used">
-            <?php echo the_field('tools_used'); ?>
+        <div class="entry-block">
+            <div class="project-navigation">
+                <h2>Contents</h2>
+                <ul>
+                    <li><a href="#desc">Description</a></li>
+                    <li><a href="#design-development">Design And Development</a></li>
+                    <li><a href="#tools">Tools Used</a></li>
+                    <li><a href="#directory">Directory Structure</a></li>
+                </ul>
+            </div>
         </div>
-        <h4 class="entry-desc" >Directory Structure</h4>
-        <div class="directory-structure">
-            <?php echo the_field('file_structure'); ?>
+        <div class="entry-block" id="description">
+            <h4 class="entry-desc" >Description</h4>
+            <p><?php echo get_the_excerpt() ?></p>
         </div>
-        
+		<div class="entry-block" id="design-development">
+            <h4 class="entry-desc" >Design And Development</h4>
+            <div class="design-img">
+                <img src="<?php echo the_field('design_image'); ?>" alt=""> 
+            </div>
+            <?php
+            the_content(
+                sprintf(
+                    wp_kses(
+                        /* translators: %s: Name of current post. Only visible to screen readers */
+                        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'luisdev' ),
+                        array(
+                            'span' => array(
+                                'class' => array(),
+                            ),
+                        )
+                    ),
+                    wp_kses_post( get_the_title() )
+                )
+            );
+            ?>
+        </div>
+        <div class="entry-block" id="tools">
+            <h4 class="entry-desc" >Tools Used</h4>
+            <div class="tools-used">
+                <?php echo the_field('tools_used'); ?>
+            </div>
+        </div>
+        <div class="entry-block" id="directory">
+            <h4 class="entry-desc" >Directory Structure</h4>
+            <div class="directory-structure">
+                <?php echo the_field('file_structure'); ?>
+            </div>
+        </div>
 	</div><!-- .entry-content -->
 </article><!-- #post-<?php the_ID(); ?> -->
