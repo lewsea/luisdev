aead6cae8654d562ff164c35914ce0d2b49060af
lewsea <luis.gudmalin@gmail.com>
2021-05-05 23:05:24 +0800

    updated about page

--- page-template/about-template.php
+++ page-template/about-template.php
@@ -16,22 +16,28 @@
         <p class="about-header">Hey 👋 I'm <span>Luis</span>
         <br>
         I am a <span>Wordpress Developer</span></p>
-        <p class='about-desc'>I'm a web developer in Philippines. It's been a year since i started doing web development. I have build various WordPress Themes from a complex <a href="">E-Commerce Theme</a> to a simple custom <a href="">Blogging Theme</a> . I also do web design and i kinda enjoy doing it. My goal is to Join or Work in any opportunity that will help me grow in this Industry. I welcome challenging projects and enjoy working with all sorts of personalities. I'm always up for any opportunities whether you have a project I can help you with, a new hiring opportunity, or just want to say hi and ask me a question, please feel free to email me at <a href="mailto:luis.gudmalin@gmail.com">luis.gudmalin@gmail.com</a></p>
+        <p class='about-desc'>I'm a web developer in Philippines. It's been a year since i started doing web development. I have build various WordPress Themes from a complex <a href="http://serene.luisgudmalin.online">Woocommerce Theme</a> to a simple custom <a href="http://harmony.luisgudmalin.online">Blogging Theme</a>. I also do web design and i kinda enjoy doing it. My goal is to Join or Work in any opportunity that will help me grow in this Industry. I welcome challenging projects and enjoy working with all sorts of personalities. I'm always up for any opportunities whether you have a project I can help you with, a new hiring opportunity, or just want to say hi and ask me a question, please feel free to email me at <a href="mailto:luis.gudmalin@gmail.com">luis.gudmalin@gmail.com</a></p>
         <h4 class="about-title">Skills</h4>
-        <ul class="about-skills">
-        <!-- add certs on each skills -->
-            <li>Wordpress (Theme Development)</li>
-            <li>HTML & (S)CSS</li>
-            <li>PHP</li>
-            <li>JavaScript (ES6+)</li>
-            <li>React</li>
-        </ul>
+        <!-- <div class="about-skills"> -->
+            <ul class="about-skills">
+            <!-- add certs on each skills -->
+                <li><a href="https://www.udemy.com/certificate/UC-W5WMMT8Y/" target="_blank">Web Development</a></li>
+                <li><a href="https://www.linkedin.com/in/luis-gudmalin-8b0349195/" target="_blank">Wordpress (Theme Development)</a></li>
+                <li><a href="https://www.freecodecamp.org/certification/lewsea/responsive-web-design" target="_blank">HTML & (S)CSS</a> </li>
+                <li><a href="https://www.linkedin.com/in/luis-gudmalin-8b0349195/" target="_blank">PHP</a></li>
+                <li><a href="https://www.freecodecamp.org/certification/lewsea/javascript-algorithms-and-data-structures" target="_blank">JavaScript (ES6+)</a></li>
+                <li><a href="https://www.udemy.com/certificate/UC-3c0927b3-db12-439d-83e1-3a5f506f63b8/" target="_blank">React</a></li> 
+            </ul>
+            <!-- <div class="certs">
+
+            </div> -->
+        <!-- </div> -->
       <h4 class="about-title">Motivation</h4>
       <p class="about-desc">"That day, if our positions were switched... Would our fates be different? Would I have your life, and you mine? Let's settle this... Dante."
       <br>
       <span class="now-im-motivated">― Vergil awaiting Dante's arrival</span></p>
-      <h4 class="about-title">Hobbies</h4>
-      <p class="about-desc">When i'm not coding or designing i usually play games in my phone or draw in my notebook mostly just a sketch when i feel motivated. I also watch anime and dramas mostly comedy and romance, or binge read the whole manga/webtoon i took interest in a day. Oh i also play <a href="https://www.chess.com/member/lewsea">Chess</a> and on <a href="https://lichess.org/@/lewsea">lichess</a>.</p>
+      <h4 class="about-title">Hobbies</h4> 
+      <p class="about-desc">When i'm not coding or designing i usually play games in my phone or draw in my notebook mostly just a sketch when i feel motivated. I also watch anime and dramas mostly comedy and romance, or binge read the whole manga/webtoon i took interest in a day. I am also studying korean language at <a href="https://talktomeinkorean.com/learningcenter/" target="_blank">TTMIK</a> and currently at level 4, though my vocabulary is good my sentence building and grammar is still a bit off. Lastly i play <a href="https://www.chess.com/member/lewsea" target="_blank">Chess</a> and <a href="https://lichess.org/@/lewsea" target="_blank">Lichess</a> to gain a bit of focus.</p>
       <h4 class="about-title">Follow</h4>
         <div class="about-socials">
             <a href="https://https//www.linkedin.com/in/luis-gudmalin-8b0349195/"><i class="fab fa-linkedin"></i> LinkedIn</a>
